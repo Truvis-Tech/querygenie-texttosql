58a7935f2ce7b4fea534d2e08255bd2080da30ab
ak-codes258 <qwertyasdf123asd@gmail.com>
2025-09-08 11:32:54 +0530

    Update random.py

--- random.py
+++ random.py
@@ -12,3 +12,12 @@ WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema', 
   "market_name": "linen-striker-454116-c9",
   "llm_type": "gpt-4-0125-preview",
   "sql_query": "SELECT * FROM linen-striker-454116-c9.techsteer.event_store\nWHERE \n  CASE \n    WHEN payment_status = 'COMPLETED' THEN 1 \n    ELSE 0 \n  END = 1\n  AND \n  CASE \n    WHEN sender_transaction_amount > 500 THEN 1 \n    ELSE 0 \n  END = 1"}
+
+curl -X 'POST' \
+  'http://localhost:8000/optimize_sql_query_by_llm' \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "market_name": "US",
+    "llm_type": "gpt-4",
+    "sql_query": "SELECT * FROM your_table LIMIT 10"
+  }'
